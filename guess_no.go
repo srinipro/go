d1323200ee32eb90d967e06065f9b7ed159830ac
Srinivasulu M <srinipro@yahoo.com>
2020-03-30 07:58:48 +0530

    Update guess_no.go

--- guess_no.go
+++ guess_no.go
@@ -15,17 +15,11 @@ func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
 func main() {
-
-	// fmt.Println("What is your MAX number to guess the system generated Random number with 10 iterations:")
-	// rdr := bufio.NewReader(os.Stdin)
-	// rdrInput, err := rdr.ReadString('\n')
-	// maxRnd, _ := strconv.Atoi(strings.TrimSpace(rdrInput))
 	var srnd = random(1, 100)
-	// if err != nil {
 	fmt.Println("Guess, What was the system generated a random number that falls between 1 to 100")
 	fmt.Println("You can discover in 10 guesses.")
 	var x int = 1
-	for x < 10 {
+	for x <= 10 {
 		fmt.Println("Guess ", x, "/10 what was system generated random number:")
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
@@ -34,6 +28,8 @@ func main() {
 		if srnd == rv {
 			fmt.Println("Good job! You guessed it!", x, "/10")
 			break
+		} else if srnd != rv && x == 10 {
+			fmt.Println("Sorry. You didn't guess my number. It was:", srnd)
 		} else if srnd > rv {
 			fmt.Println("Oops. Your guess was LOW")
 		} else if srnd < rv {
@@ -42,5 +38,4 @@ func main() {
 		x = x + 1
 
 	}
-	// }
 }
